1a144ff563fb60f329845ba743bc32976f2d7030
kombucha7 <29064557+kombucha7@users.noreply.github.com>
2023-10-26 22:48:36 +0800

    Update job.sh

--- job.sh
+++ job.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --mem=8G
-#SBATCH --job-name=elliot_job_2610_2129 
+#SBATCH --job-name=elliot_job_2610_2158 
 #SBATCH --output=output_%x_%j.out 
 #SBATCH --error=error_%x_%j.err
 
